e3b3d6a4d039878d06fdbce2c1ea0543ccf9b7ae
Vivek <vivek.rbj.up@gmail.com>
2023-10-31 22:01:55 +0530

    find update delete

--- User.js
+++ User.js
@@ -1,13 +1,63 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 // const mongoose = require("mongoose")
 
 
 //1.2
 //create schema
+
+// const userSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number
+// })
+
+const addressSchema = new mongoose.Schema({
+    street: String,
+    city: String
+});
+
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number
-})
+    age: {
+        type: Number,
+        min: 1,
+        max: 1000,
+        validate: {
+            //age should always be even
+            validator: v => v%2 === 0,
+            message: props => `${props.value} is not an even number`
+        }
+    },
+    // email: String,
+    email: {
+        type: String,
+        minlength: 10,
+        required: true,
+        lowercase: true
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now()
+    },
+    // represent another user
+    bestFriend: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    hobbies: [String],
+    address: addressSchema
+    // address: {
+    //     street: String,
+    //     city: String
+    // }
+});
+
+
 
 //create model
 const User = mongoose.model("User", userSchema)
--- index.js
+++ index.js
@@ -16,16 +16,74 @@ mongoose.connect("mongodb://127.0.0.1:27017/crudDB", {useNewUrlParser: true, use
 // user.save().then(() => console.log("User saved"));
 // console.log(user);
 
-async function run(){
-    // const user = new User({name: "John", age: 18 })
-    // await user.save()
+// async function run(){
+//     // const user = new User({name: "John", age: 18 })
+//     // await user.save()
 
-    const user = await User.create({name: "John", age: 19});
-    // update name
-    user.name = "Wick"
-    await user.save()
+//     const user = await User.create({name: "John", age: 19});
+//     // update name
+//     user.name = "Wick"
+//     await user.save()
 
-    console.log("Data inserted" + user);
-}
+//     console.log("Data inserted" + user);
+// }
 
-run()
+
+// create user in new schema
+
+// async function run(){
+//     try{
+//         const user = await User.create({
+//             name: "Wick",
+//             age: 20,
+//             email: "test@gmail.com",
+//             hobbies: ["reading", "cooking"],
+//             address: {
+//                 street: "Main street"
+//             }
+//         });
+//         console.log(user);
+//     }
+//     catch(err){
+//         // console.log(err);
+//         console.log(err.message);
+//     }
+// }
+
+// run()
+
+//updateOne and updateMany methods don't go though validation so prefer using findById methods
+
+// console.log(User.findById());
+
+// async function UPdate() {
+//     try{
+//         // const user = await User.findById("6540f6caedce448080e2eb30");
+
+//         // find() is same as mongodb
+//         // const user = await User.find({name: "Sam"});
+
+//         // READ using where method
+//         // const user = await User.where("name").equals("Sam");
+//         // const user = await User.where("age").gt(15);
+//         // const user = await User.where("age").gt(15).where("name").equals("Wick");
+//         // const user = await User.where("age").gt(15).where("name").equals("Sam").limit(2);
+//         // const user = await User.where("age").gt(15).where("name").equals("Sam").select("age");
+//         const user = await User.where("name").equals("Sam").populate("bestFriend");
+
+//         // user[0].bestFriend = "6540fc0149d43b71883b4705";
+//         // await user[0].save();
+        
+//         // deleteOne is used to delete only one tuple but deleteMany will delete all the required tuples
+//         // const user = await User.deleteOne({name: "Sam"});
+
+//         console.log(user);
+//     }
+//     catch(err){
+//         console.log(err.message);
+//     }
+// }
+
+// UPdate();
+
+
